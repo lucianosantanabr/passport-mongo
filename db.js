9825160fbf0b077346e814f9199c07c03de30850
Luciano Santana <luciano@localhost.localdomain>
2015-10-28 21:33:53 -0200

    add

---
+++ db.js
@@ -0,0 +1,23 @@
+module.exports = {
+	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
+	'url' : 'mongodb://localhost/test'
+}
+
+/*
+use strict';
+
+const mongo = require('mongodb'),
+      monk  = require('monk'),
+      db    = monk('127.0.0.1:27017/task');
+
+module.exports = (app) => {
+  app.use((req, res, next) => {
+    let url = req.url.substring(1),
+        baseUrl = url.substring(0, url.indexOf('/')) || url;
+
+    res.locals.db = db.get(baseUrl);
+    //req.db = db;
+    next();
+  });
+};
+*/
